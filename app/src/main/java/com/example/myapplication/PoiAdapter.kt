60181dd2f70e04321517f42a5c4bc11c4259dfb0
juannn89 <90584086+juannn89@users.noreply.github.com>
2021-11-19 17:39:35 -0500

    creación de RecyclerView

---
+++ app/src/main/java/com/example/myapplication/PoiAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PoiAdapter(val pois:List<Poi>):RecyclerView.Adapter<PoiAdapter.ViewHolder>() {
+
+    class ViewHolder(v: View):RecyclerView.ViewHolder(v){
+        var nombre: TextView
+        var descripcion: TextView
+        var sitio: ImageView
+
+        init {
+            nombre = v.findViewById(R.id.tvNombre)
+            descripcion = v.findViewById(R.id.tvDescripcion)
+            sitio = v.findViewById(R.id.ivPoi)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PoiAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: PoiAdapter.ViewHolder, position: Int) {
+        val p = pois[position]
+        holder.nombre.text = p.nombre
+        holder.descripcion.text = p.descripcion
+        holder.sitio.setImageResource(p.sitio)
+    }
+
+    override fun getItemCount(): Int {
+        return pois.size
+    }
+
+}
